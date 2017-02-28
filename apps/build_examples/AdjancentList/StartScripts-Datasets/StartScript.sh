4e45eb11b474834381f33e3d33db93f6adc5c0d3
Ring3 <328149598@qq.com>
2017-03-01 00:23:31 +0800

    Create StartScript.sh

---
+++ apps/build_examples/AdjancentList/StartScripts-Datasets/StartScript.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#create data
+size1=1000000
+size2=1000
+java -classpath $ADJ_HOME/StartScripts-Datasets/ CreateAdjListData -size $size1
+#start job
+./bin/spark-submit --master=spark://10.22.1.1:10086 $ADJ_HOME/target/scala-2.11/adjancentlist_2.11-1.0.0.jar $size2 /home/wfan/DFT-Project/apps/build_examples/AdjancentList/java/StartScripts-Datasets/combined $ADJ_HOME/java/result true
