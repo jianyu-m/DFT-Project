9cbdeb4a057c0f5f492c1b832903d470f7a29f6e
Ring3 <328149598@qq.com>
2017-03-01 10:53:02 +0800

    Update StartScript.sh

--- apps/build_examples/PreCount/StartScripts-Datasets/StartScript.sh
+++ apps/build_examples/PreCount/StartScripts-Datasets/StartScript.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-cd $PRE_HOME/StartScripts-Datasets
-./bin/spark-submit --deploy-mode=cluster --master=spark://10.22.1.1:10086 /home/wfan/DFT-Project/apps/build_examples/PreCount/target/scala-2.11/prefixcount_2.11-1.0.0.jar 
+minlen=1
+maxlen=10
+inputpath=$PRE_HOME/StartScripts-Datasets/dataset1
+outputpath="./result"
+ispartitioned=true
+cd ~/spark-2.1.0-bin-hadoop2.7
+./bin/spark-submit --deploy-mode=cluster --master=spark://10.22.1.1:10086 /home/wfan/DFT-Project/apps/build_examples/PreCount/target/scala-2.11/prefixcount_2.11-1.0.0.jar $maxlen $minlen $inputpath $outputpath $ispartitioned
