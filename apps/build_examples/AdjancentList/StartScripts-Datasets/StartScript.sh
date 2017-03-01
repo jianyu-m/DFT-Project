256b2dd2949cb1746398f644f13f428f4712911c
Ring3 <328149598@qq.com>
2017-03-01 10:56:56 +0800

    Update StartScript.sh

--- apps/build_examples/AdjancentList/StartScripts-Datasets/StartScript.sh
+++ apps/build_examples/AdjancentList/StartScripts-Datasets/StartScript.sh
@@ -2,7 +2,9 @@
 #create data
 size1=1000000
 iterrounds=1000
+outputpath="result"
+ispartitioned=true
 cd ~/spark-2.1.0-bin-hadoop2.7
 java -classpath $ADJ_HOME/StartScripts-Datasets/ CreateAdjListData -size $size1
 #start job
-./bin/spark-submit --master=spark://10.22.1.1:10086 $ADJ_HOME/target/scala-2.11/adjancentlist_2.11-1.0.0.jar $iterrounds /home/wfan/DFT-Project/apps/build_examples/AdjancentList/java/StartScripts-Datasets/combined $ADJ_HOME/java/result true
+./bin/spark-submit --master=spark://10.22.1.1:10086 $ADJ_HOME/target/scala-2.11/adjancentlist_2.11-1.0.0.jar $iterrounds /home/wfan/DFT-Project/apps/build_examples/AdjancentList/java/StartScripts-Datasets/combined $ADJ_HOME/StartScripts-Datasets/$outputpath $ispartitioned
